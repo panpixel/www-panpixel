91e4863fec5304ddc2e18ca297ea9fbecd9d9a9d
panpixel <do@panpixel.com>
2021-02-12 22:23:05 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,10 +42,7 @@ export default {
         '~/plugins/vue-awesome-swiper.js',
         '~/plugins/vuejs-paginate.js',
         '~/plugins/vue2-google-maps.js',
-        '~/plugins/vue-masonry-css.js',
-        '~/plugins/Mixitup.client.js',
-        '~/plugins/silentbox.js',
-        { src: '~/plugins/vue-masonry', ssr: false },
+
         { src:'~/plugins/ga.js', mode: 'client' }
     ],
     /*
