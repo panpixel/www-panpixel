0605d2713939b2791e7658e432555b3f3488792d
panpixel <do@panpixel.com>
2021-04-21 22:30:11 +0200

    1

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,47 @@ export default {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+            {
+                hid: 'og:type',
+                property: 'og:type',
+                content: 'website'
+              },
+              {
+                hid: 'og:title',
+                property: 'og:title',
+                content: 'Filmy reklamowe ⋆ Wirtualny spacer ⋆ Tworzenie stron WWW'
+              },
+              {
+                hid: 'og:description',
+                property: 'og:description',
+                content: 'Za pomocą obrazu i dźwięku wzbogaconego o odpowiedni tekst, skutecznie docieramy do określonej grupy odbiorców. Tworzymy filmy reklamowe'
+              },
+              {
+                hid: 'og:image',
+                property: 'og:image',
+                content: 'https://www.panpixel.studio/img/slider/film-reklamowy-dla-biznesu.jpg' // the size has to be more 200px at least 
+              },
+              {
+                hid: 'og:url',
+                property: 'og:url',
+                content: 'https://www.panpixel.studio/'
+              },
+              {
+                hid: 'og:site_name',
+                property: 'og:site_name',
+                content: 'panpixel.studio'
+              },
+              {
+                hid: 'og:locale',
+                property: 'og:locale',
+                content: 'pl'
+              },
+              {
+                hid: 'og:image:type',
+                property: 'og:image:type',
+                content: 'image/jpeg'
+              },
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
